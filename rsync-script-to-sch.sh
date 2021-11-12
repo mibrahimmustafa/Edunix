0aa4a6d07ac70df71a3987e796f6073f10b72702
= <=>
2021-11-13 01:34:02 +0300

    update code files

--- rsync-script-to-sch.sh
+++ rsync-script-to-sch.sh
@@ -2,14 +2,15 @@
 
 export PATH=/usr/local/bin:/usr/bin:/bin
 DAY=`date +"%d-%m-%Y"`
-
-list_to_backup=[/etc , /home,/media,/mnt,/opt,/root,/var]
-
-for d in $list_to_backup; do
-
-rsync -azvh $d /data2/backup
-
+mkdir backup
+list_to_backup=`ls / `
+list_to_backup1={'home','opt','root','var'}
+for i in $list_to_backup; do
+if $i in list_to_backup1; then
+rsync -avzh /$i /data2/backup/
+fi
 done
 cd /data2
 
-compress `tar -cvf /data2/backup backup_$DAY.tar`
+
+zip  backup* .
